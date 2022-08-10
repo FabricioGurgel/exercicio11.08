2e991a7d3a5c003a39f3435a231ef21f5591aa13
FabricioGurgel <fabriciogurgel.85@gmail.com>
2022-08-10 20:44:36 -0300

    calculador de media

---
+++ exercicioVariaveis11.08/src/exercicioVariavel1108/exercicioVariaveis1108.java
@@ -0,0 +1,17 @@
+package exercicioVariavel1108;
+
+public class exercicioVariaveis1108 {
+
+	public static void main(String[] args) {
+		double media1 = 8.9;
+		double media2 = 7.5;
+		double media3 = 8.5;
+		double media4 = 7.7;
+		double media;
+		media = (media1 + media2 + media3 + media4)/4;
+		
+		System.out.println(media);
+				
+	}
+
+}
