f2af5f4b8e41013f4a093f321c7630ae724d7e66
FabricioGurgel <fabriciogurgel.85@gmail.com>
2022-08-21 12:39:56 -0300

    exercicio sobre if, else, else if

---
+++ exercicioVariaveis11.08/src/aulasIf/ExercicioIf1.java
@@ -0,0 +1,23 @@
+package aulasIf;
+
+public class ExercicioIf1 {
+
+	public static void main(String[] args) {
+		if(9 > 10) {
+			System.out.println("verdade1");
+			
+		}
+		else if (8 > 10) {
+			System.out.println("verdade2");
+			
+		}
+		else if (10 > 12 || 5 < 2) {
+				System.out.println("verdade3");
+			}
+		else {System.out.println("todas sao falsas");
+			}
+		
+		
+	}
+
+}
