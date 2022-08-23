a1a9e8b612b73e218d1b23adfe6036f25d53d831
FabricioGurgel <fabriciogurgel.85@gmail.com>
2022-08-22 22:14:38 -0300

    identação

--- exercicioVariaveis11.08/src/aulasIf/ExercicioIf1.java
+++ exercicioVariaveis11.08/src/aulasIf/ExercicioIf1.java
@@ -3,21 +3,18 @@ package aulasIf;
 public class ExercicioIf1 {
 
 	public static void main(String[] args) {
-		if(9 > 10) {
+		if (11 > 10) {
 			System.out.println("verdade1");
-			
-		}
-		else if (8 > 10) {
+
+		} else if (8 > 10) {
 			System.out.println("verdade2");
-			
+
+		} else if (10 > 12 || 5 < 2) {
+			System.out.println("verdade3");
+		} else {
+			System.out.println("todas sao falsas");
 		}
-		else if (10 > 12 || 5 < 2) {
-				System.out.println("verdade3");
-			}
-		else {System.out.println("todas sao falsas");
-			}
-		
-		
+
 	}
 
 }
